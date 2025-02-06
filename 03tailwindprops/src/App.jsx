e286cbe0a7eced74ee84cf0caf1b7bed39079535
rimo02 <ghoshrimo02@gmail.com>
2025-02-06 14:11:13 +0530

    new commits

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Card from "./components/card";
 function App() {
   let myObj = {
@@ -9,7 +8,7 @@ function App() {
     <>
       <h1 className="text-sky-200 mb-4">Chai and React</h1>
       <Card username={myObj.username} text="Bored Ape" />
-      <Card username={"3b1b"} text="Crypto Punks"/>
+      <Card username={"3b1b"} text="Crypto Punks" />
     </>
   );
 }
--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("#212121");
+  const defaultColor = "#212121";
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen duration-100"
+        style={{ backgroundColor: color }}
+      >
+        {/* Container to center the button div */}
+        <div className="w-1/2">
+          <div className="flex flex-wrap fixed bottom-10 p-2 bg-black inset-x-0 justify-center">
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "red" }}
+              onClick={() => setColor("red")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "blue" }}
+              onClick={() => setColor("blue")}
+            >
+              Blue
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "yellow" }}
+              onClick={() => setColor("yellow")}
+            >
+              Yellow
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "green" }}
+              onClick={() => setColor("green")}
+            >
+              Green
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "pink" }}
+              onClick={() => setColor("pink")}
+            >
+              Pink
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "olive" }}
+              onClick={() => setColor("olive")}
+            >
+              Olive
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: "beige" }}
+              onClick={() => setColor("beige")}
+            >
+              Beige
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl"
+              style={{ backgroundColor: defaultColor }}
+              onClick={() => setColor(defaultColor)}
+            >
+              Default
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
